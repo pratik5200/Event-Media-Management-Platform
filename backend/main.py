14077f9361e6e4a42566bea4744f3404f7073792
pratik5200 <pratikjaisal@gmail.com>
2026-06-09 18:07:53 +0530

    fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -64,7 +64,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins, 
-    allow_origin_regex="https://.*\.vercel\.app", 
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,  
     allow_methods=["*"],     
     allow_headers=["*"],     
